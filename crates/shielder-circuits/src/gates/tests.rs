809f88c197a0d505daf0ee9a2ec33dd7ef864892
Piotr Mikołajczyk <piomiko41@gmail.com>
2025-01-03 15:07:17 +0100

    Docs

--- crates/shielder-circuits/src/gates/tests.rs
+++ crates/shielder-circuits/src/gates/tests.rs
@@ -7,12 +7,25 @@ use halo2_proofs::{
 
 use crate::{column_pool::ColumnPool, embed::Embed, gates::Gate, Field};
 
+/// The minimal circuit that uses a single gate. It represents a single application of the gate to
+/// the input.
+///
+/// # Configuration phase
+///
+/// The constraint system will register exactly one gate (after creating required columns).
+///
+/// # Synthesis phase
+///
+/// The circuit will perform two steps:
+/// 1. Embed the input into the circuit.
+/// 2. Apply the gate to the embedded input.
 pub struct OneGateCircuit<Gate, Input> {
     input: Input,
     _marker: PhantomData<Gate>,
 }
 
 impl<Gate, Input> OneGateCircuit<Gate, Input> {
+    /// Create new circuit instance for a single gate application to `input`.
     pub fn new(input: Input) -> Self {
         Self {
             input,
@@ -34,12 +47,15 @@ impl<F: Field, G: Gate<F> + Clone, Input: Embed<F, Embedded = <G as Gate<F>>::In
         }
     }
 
+    /// Our only goal is to register gate `G`. Firstly, we organize sufficient advice area and then
+    /// we create the gate instance.
     fn configure(meta: &mut ConstraintSystem<F>) -> Self::Config {
         let mut advice_pool = ColumnPool::<Advice>::new();
         let advice = G::organize_advices(&mut advice_pool, meta);
         (advice_pool, G::create_gate(meta, advice))
     }
 
+    /// Embed the input and apply the gate.
     fn synthesize(
         &self,
         (advice_pool, gate): (ColumnPool<Advice>, G),
