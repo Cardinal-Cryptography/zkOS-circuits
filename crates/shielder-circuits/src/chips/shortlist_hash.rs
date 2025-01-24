95fa6d59e951580796c8bf1674e6a403420d39e5
Piotr Mikołajczyk <piomiko41@gmail.com>
2025-01-24 13:28:08 +0100

    split tests

--- crates/shielder-circuits/src/chips/shortlist_hash.rs
+++ crates/shielder-circuits/src/chips/shortlist_hash.rs
@@ -87,6 +87,60 @@ pub mod off_circuit {
     }
 }
 
+#[cfg(test)]
+mod off_circuit_tests {
+    use std::array;
+
+    use crate::{
+        chips::shortlist_hash::{off_circuit::shortlist_hash, Shortlist},
+        poseidon::off_circuit::hash,
+        Fr,
+    };
+
+    #[test]
+    fn test_single_chunk_hash() {
+        let input: Shortlist<Fr, 6> = Shortlist::new(array::from_fn(|i| (i as u64).into()));
+
+        let expected_hash = hash(&[
+            Fr::from(0),
+            Fr::from(1),
+            Fr::from(2),
+            Fr::from(3),
+            Fr::from(4),
+            Fr::from(5),
+            Fr::zero(),
+        ]);
+
+        assert_eq!(expected_hash, shortlist_hash(&input));
+    }
+
+    #[test]
+    fn test_chained_hash() {
+        let input: Shortlist<Fr, 12> = Shortlist::new(array::from_fn(|i| (i as u64).into()));
+
+        let hash_chunk_2 = hash(&[
+            Fr::from(6),
+            Fr::from(7),
+            Fr::from(8),
+            Fr::from(9),
+            Fr::from(10),
+            Fr::from(11),
+            Fr::from(0),
+        ]);
+        let expected_hash = hash(&[
+            Fr::from(0),
+            Fr::from(1),
+            Fr::from(2),
+            Fr::from(3),
+            Fr::from(4),
+            Fr::from(5),
+            hash_chunk_2,
+        ]);
+
+        assert_eq!(expected_hash, shortlist_hash(&input));
+    }
+}
+
 /// Chip that is able to calculate the shortlist hash
 #[derive(Clone, Debug)]
 pub struct ShortlistHashChip<const N: usize> {
@@ -121,7 +175,7 @@ impl<const N: usize> ShortlistHashChip<N> {
 }
 
 #[cfg(test)]
-mod test {
+mod chip_tests {
     use std::{array, vec};
 
     use assert2::assert;
@@ -136,7 +190,6 @@ mod test {
         column_pool::{ColumnPool, PreSynthesisPhase},
         config_builder::ConfigsBuilder,
         embed::Embed,
-        poseidon::off_circuit::hash,
         synthesizer::create_synthesizer,
         AssignedCell, Fr,
     };
@@ -207,48 +260,5 @@ mod test {
             .verify();
 
         assert!(result.is_ok());
-    }
-
-    #[test]
-    fn test_chained_hash() {
-        let input: Shortlist<Fr, 12> = Shortlist::new(array::from_fn(|i| (i as u64).into()));
-
-        let hash_chunk_2 = hash(&[
-            Fr::from(6),
-            Fr::from(7),
-            Fr::from(8),
-            Fr::from(9),
-            Fr::from(10),
-            Fr::from(11),
-            Fr::from(0),
-        ]);
-        let expected_hash = hash(&[
-            Fr::from(0),
-            Fr::from(1),
-            Fr::from(2),
-            Fr::from(3),
-            Fr::from(4),
-            Fr::from(5),
-            hash_chunk_2,
-        ]);
-
-        assert_eq!(expected_hash, shortlist_hash(&input));
-    }
-
-    #[test]
-    fn test_single_chunk_hash() {
-        let input: Shortlist<Fr, 6> = Shortlist::new(array::from_fn(|i| (i as u64).into()));
-
-        let expected_hash = hash(&[
-            Fr::from(0),
-            Fr::from(1),
-            Fr::from(2),
-            Fr::from(3),
-            Fr::from(4),
-            Fr::from(5),
-            Fr::zero(),
-        ]);
-
-        assert_eq!(expected_hash, shortlist_hash(&input));
     }
 }
