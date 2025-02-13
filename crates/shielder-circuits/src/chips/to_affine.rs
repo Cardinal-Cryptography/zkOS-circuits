98157c7ff9def0d2351f6503d3d7fe8a43707577
filip <fbielejec@gmail.com>
2025-02-13 13:37:08 +0100

    fix

--- crates/shielder-circuits/src/chips/to_affine.rs
+++ crates/shielder-circuits/src/chips/to_affine.rs
@@ -2,10 +2,11 @@ use halo2_proofs::{circuit::Value, plonk::Error};
 
 use crate::{
     consts::GRUMPKIN_3B,
-    curve_arithmetic::{self, GrumpkinPoint},
+    curve_arithmetic::{self, GrumpkinPoint, GrumpkinPointAffine},
     embed::Embed,
     gates::{
         point_double::{PointDoubleGate, PointDoubleGateInput},
+        to_affine::ToAffineGate,
         Gate,
     },
     synthesizer::Synthesizer,
@@ -16,4 +17,16 @@ use crate::{
 pub struct ToAffineChipInput<T> {
     pub point_projective: GrumpkinPoint<T>,
     pub point_projective_z_inverse: T,
+}
+
+#[allow(dead_code)]
+#[derive(Copy, Clone, Debug)]
+pub struct ToAffineChipOutput<T> {
+    pub point_affine: GrumpkinPointAffine<T>,
+}
+
+/// Chip that converts a point in projective coordinates to affine coordinates.
+#[derive(Clone, Debug)]
+pub struct ToAffineChip {
+    pub gate: ToAffineGate,
 }
--- crates/shielder-circuits/src/gates/to_affine.rs
+++ crates/shielder-circuits/src/gates/to_affine.rs
@@ -3,7 +3,7 @@ use alloc::vec;
 use halo2_proofs::{
     arithmetic::Field,
     halo2curves::bn256::Fr,
-    plonk::{Advice, Column, ConstraintSystem, Constraints, Error, Expression, Selector},
+    plonk::{Advice, Column, ConstraintSystem, Constraints, ErrorFront, Expression, Selector},
     poly::Rotation,
 };
 use macros::embeddable;
@@ -105,7 +105,7 @@ impl Gate for ToAffineGate {
             point_affine,
             point_projective_z_inverse: z_inverse,
         }: Self::Input,
-    ) -> Result<(), Error> {
+    ) -> Result<(), ErrorFront> {
         synthesizer.assign_region(
             || GATE_NAME,
             |mut region| {
