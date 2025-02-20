8824cd034e5061594d6f0d69cb0bdc76d2f399d8
filip <fbielejec@gmail.com>
2025-02-20 16:30:28 +0100

    knowledge

--- crates/shielder-circuits/src/circuits/new_account/knowledge.rs
+++ crates/shielder-circuits/src/circuits/new_account/knowledge.rs
@@ -66,18 +66,16 @@ impl PublicInputProvider<NewAccountInstance> for NewAccountProverKnowledge<Fr> {
             .sqrt()
             .expect("element has a square root");
 
-        let point = GrumpkinPointAffine::new(symmetric_key, y).into();
         // TODO: in production there should be a separate trapdoor field element for the symmetric key encryption
         let trapdoor_le_bits = field_element_to_le_bits(self.trapdoor);
 
-        let input = ElGamalEncryptionInput {
-            message: point,
+        let (c1, c2) = asymmetric_encryption::off_circuit::encrypt(ElGamalEncryptionInput {
+            message: GrumpkinPointAffine::new(symmetric_key, y).into(),
             public_key: self.anonymity_revoker_public_key,
             trapdoor_le_bits,
-        };
-
-        // TODO : take these two points to affine coords
-        let (ciphertext1, ciphertext2) = asymmetric_encryption::off_circuit::encrypt(input);
+        });
+        let ciphertext1: GrumpkinPointAffine<Fr> = c1.into();
+        let ciphertext2: GrumpkinPointAffine<Fr> = c2.into();
 
         match instance_id {
             NewAccountInstance::HashedNote => note_hash(&Note {
@@ -93,14 +91,10 @@ impl PublicInputProvider<NewAccountInstance> for NewAccountProverKnowledge<Fr> {
             NewAccountInstance::TokenAddress => self.token_address,
             NewAccountInstance::AnonymityRevokerPublicKeyX => self.anonymity_revoker_public_key.x,
             NewAccountInstance::AnonymityRevokerPublicKeyY => self.anonymity_revoker_public_key.y,
-            NewAccountInstance::SymKeyEncryptionCiphertext1X => todo!(),
-            NewAccountInstance::SymKeyEncryptionCiphertext1Y => todo!(),
-            NewAccountInstance::SymKeyEncryptionCiphertext2X => todo!(),
-            NewAccountInstance::SymKeyEncryptionCiphertext2Y => todo!(),
-            // NewAccountInstance::SymKeyEncryption => asymmetric_encryption::off_circuit::encrypt(
-            //     self.anonymity_revoker_public_key,
-            //     sym_key::off_circuit::derive(self.id),
-            // ),
+            NewAccountInstance::SymKeyEncryptionCiphertext1X => ciphertext1.x,
+            NewAccountInstance::SymKeyEncryptionCiphertext1Y => ciphertext1.y,
+            NewAccountInstance::SymKeyEncryptionCiphertext2X => ciphertext2.x,
+            NewAccountInstance::SymKeyEncryptionCiphertext2Y => ciphertext2.y,
         }
     }
 }
